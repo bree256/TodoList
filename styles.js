c5a801133f2de1be6b2ea5e2e4464628077b48a8
bree256 <141923968+bree256@users.noreply.github.com>
2024-02-10 19:18:54 +0300

    first commit

---
+++ styles.js
@@ -0,0 +1,14 @@
+const styles = new CSSStyleSheet();
+styles.replaceSync(
+  `.question-wrapper {
+    color:red;
+  }
+  ul {
+        list-style: none;
+      }
+      
+      todo-item input[type="checkbox"] {
+        margin-right: 10px;
+        border-radius: 50%;
+      }`
+);
